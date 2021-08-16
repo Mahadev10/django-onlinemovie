d395a31143440b623575588bab9a1fc90545ae70
Mahadev <Mahadev.clever@gmail.com>
2021-08-16 19:17:32 +0530

    home page updated

--- movieapp/views.py
+++ movieapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from .models import Movie
 # Create your views here.
 def home(request):
-    return render(request,"home.html",{})
+    movie_data=Movie.objects.filter(show=True)
+    print(movie_data)
+    return render(request,"home.html",context={"movies":movie_data})
 
 def testimonials(request):
     return render(request,"testimonials.html",{})
