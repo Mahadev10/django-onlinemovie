fbc255aef80e7405fd5b617df1a373115173038a
Mahadev <Mahadev.clever@gmail.com>
2021-08-16 18:28:30 +0530

    Movie admin updated

--- movieapp/admin.py
+++ movieapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Movie
 # Register your models here.
+class MovieAdmin(admin.ModelAdmin):
+    list_display=('name','year','star','show')
+    list_filter=('star','show')
+admin.site.register(Movie,MovieAdmin)
